e1def7df437537c7794c8608220d871c0988c93b
geon <kky8586696@naver.com>
2025-03-20 12:09:10 +0900

    refactor: JsonPreperty 어노테이션 제거

--- src/InterviewAssistant.Common/Models/ChatRequest.cs
+++ src/InterviewAssistant.Common/Models/ChatRequest.cs
@@ -1,19 +1,13 @@
-using System.Text.Json.Serialization;
-
 namespace InterviewAssistant.Common.Models
 {
     public class ChatRequest
     {   
-        [JsonPropertyName("messages")]
         public List<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
     }
 
     public class ChatMessage
     {
-        [JsonPropertyName("role")]
         public string Role { get; set; } = string.Empty;
-        
-        [JsonPropertyName("message")]
         public string Message { get; set; } = string.Empty;
     }
 }
--- src/InterviewAssistant.Common/Models/ChatResponse.cs
+++ src/InterviewAssistant.Common/Models/ChatResponse.cs
@@ -1,9 +1,6 @@
-using System.Text.Json.Serialization;
-
 namespace InterviewAssistant.Common.Models;
 
 public class ChatResponse
 {
-    [JsonPropertyName("message")]
     public string Message { get; set; } = string.Empty;
 }
