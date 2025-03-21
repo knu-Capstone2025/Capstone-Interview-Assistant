b0035eb7cd184f4107ec49ab937e579d105ca9eb
Justin Yoo <justin.yoojh@gmail.com>
2025-03-21 14:56:12 +0900

    각종 정리 (#16)
    - NuGet 패키지 버전 정리
    - 공통 라이브러리 XML 코멘트 추가

--- src/InterviewAssistant.Common/Models/InterviewDataRequest.cs
+++ src/InterviewAssistant.Common/Models/InterviewDataRequest.cs
@@ -1,8 +1,17 @@
 namespace InterviewAssistant.Common.Models;
 
+/// <summary>
+/// This represents the request entity for the interview data service. It contains the URL of the resume and the job description that are used to generate interview questions and answers.
+/// </summary>
 public class InterviewDataRequest
 {
+    /// <summary>
+    /// Gets or sets the URL of the resume document.
+    /// </summary>
     public string ResumeUrl { get; set; } = string.Empty;
 
-    public string JobPostingUrl { get; set; } = string.Empty;
+    /// <summary>
+    /// Gets or sets the URL of the job description document.
+    /// </summary>
+    public string JobDescriptionUrl { get; set; } = string.Empty;
 }
