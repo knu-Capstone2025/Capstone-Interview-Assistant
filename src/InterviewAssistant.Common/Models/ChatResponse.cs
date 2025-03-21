e5ae3919257c3f220e016bbf49ff8ead4071df67
Justin Yoo <justin.yoojh@gmail.com>
2025-03-21 09:43:01 +0900

    각종 정리
    - NuGet 패키지 버전 정리
    - 공통 라이브러리 XML 코멘트 추가

--- src/InterviewAssistant.Common/Models/ChatResponse.cs
+++ src/InterviewAssistant.Common/Models/ChatResponse.cs
@@ -1,6 +1,12 @@
 namespace InterviewAssistant.Common.Models;
 
+/// <summary>
+/// This represents the response entity for the chat service. It contains a message that is sent from the assistant to the user.
+/// </summary>
 public class ChatResponse
 {
+    /// <summary>
+    /// Gets or sets the message that is sent from the assistant to the user.
+    /// </summary>
     public string Message { get; set; } = string.Empty;
 }
