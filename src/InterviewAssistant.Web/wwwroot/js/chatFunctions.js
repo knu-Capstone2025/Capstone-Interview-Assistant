3ccdbfc8e6a48ef55b39524613adb510d2d62948
박신영 <psy_chopath@naver.com>
2025-03-21 15:13:50 +0900

    fix: 빈 입력창에 엔터키 입력시 직전 입력값 전송되는 버그 수정

--- src/InterviewAssistant.Web/wwwroot/js/chatFunctions.js
+++ src/InterviewAssistant.Web/wwwroot/js/chatFunctions.js
@@ -1,16 +1,21 @@
 // chatFunctions.js
-window.scrollToBottomWithOffset = function(elementId, offset) {
-  const element = document.getElementById(elementId);
-  if (element) {
-      element.scrollTop = element.scrollHeight - offset;
-  }
-}
+window.scrollToBottomWithOffset = function (elementId, offset) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.scrollTop = element.scrollHeight - offset;
+    }
+};
 
-window.focusTextArea = function(elementId) {
-  setTimeout(function() {
-      const element = document.getElementById(elementId);
-      if (element) {
-          element.focus();
-      }
-  }, 0);
-}
+window.focusTextArea = function (elementId) {
+    setTimeout(function () {
+        const element = document.getElementById(elementId);
+        if (element) {
+            element.focus();
+        }
+    }, 0);
+};
+
+window.getTextAreaValue = function (elementId) {
+    const element = document.getElementById(elementId);
+    return element ? element.value : "";
+};
