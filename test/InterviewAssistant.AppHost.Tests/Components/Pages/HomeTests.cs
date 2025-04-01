d17688b704588e95cffe22a61738c658f6b907f8
choi-sunggyu <456tjdrb@naver.com>
2025-04-01 18:50:30 +0900

    chore: 이력서 링크를 URL 공유가 아닌 로컬파일 업로드로 수정, messages Role을 Assistant에서 System으로 수정

--- test/InterviewAssistant.AppHost.Tests/Components/Pages/HomeTests.cs
+++ test/InterviewAssistant.AppHost.Tests/Components/Pages/HomeTests.cs
@@ -263,7 +263,7 @@ namespace InterviewAssistant.AppHost.Tests.Components.Pages
 
             // Assert
             await alertHandled.Task;
-            alertMessage.ShouldBe("URL이 유효하지 않습니다. 다시 확인해주세요.");
+            alertMessage.ShouldBe("이력서 파일과 채용공고 URL을 올바르게 입력해주세요.");
         }
 
         /// <summary>
