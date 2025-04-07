d46ce155adad959b513710434d924dc93dc6a219
dkswoals <a4445656@gmail.com>
2025-04-07 12:40:59 +0900

    Refactor: 지시문 위치 수정

--- test/InterviewAssistant.ApiService.Tests/Services/KernelServiceTest.cs
+++ test/InterviewAssistant.ApiService.Tests/Services/KernelServiceTest.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Shouldly;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
@@ -14,120 +16,118 @@ using NSubstitute;
 
 using NUnit.Framework;
 
-using Shouldly;
-
 using InterviewAssistant.ApiService.Services;
 
-namespace InterviewAssistant.ApiService.Tests.Services
+namespace InterviewAssistant.ApiService.Tests.Services;
+
+[TestFixture]
+public class KernelServiceTests
 {
-    [TestFixture]
-    public class KernelServiceTests
+    private Kernel _kernel;
+    private IChatCompletionService _chatCompletionService;
+    private IConfiguration _configuration;
+    private KernelService _kernelService;
+    private const string ServiceId = "testServiceId";
+
+    [SetUp]
+    public void Setup()
     {
-        private Kernel _kernel;
-        private IChatCompletionService _chatCompletionService;
-        private IConfiguration _configuration;
-        private KernelService _kernelService;
-        private const string ServiceId = "testServiceId";
+        // Create substitutes
+        _chatCompletionService = Substitute.For<IChatCompletionService>();
+        _configuration = Substitute.For<IConfiguration>();
 
-        [SetUp]
-        public void Setup()
-        {
-            // Create substitutes
-            _chatCompletionService = Substitute.For<IChatCompletionService>();
-            _configuration = Substitute.For<IConfiguration>();
+        // Setup configuration
+        _configuration["SemanticKernel:ServiceId"].Returns(ServiceId);
 
-            // Setup configuration
-            _configuration["SemanticKernel:ServiceId"].Returns(ServiceId);
-            
-            // Create a kernel with our substitute service
-            var builder = Kernel.CreateBuilder();
-            builder.Services.AddKeyedSingleton<IChatCompletionService>(ServiceId, _chatCompletionService);
-            _kernel = builder.Build();
+        // Create a kernel with our substitute service
+        var builder = Kernel.CreateBuilder();
+        builder.Services.AddKeyedSingleton<IChatCompletionService>(ServiceId, _chatCompletionService);
+        _kernel = builder.Build();
 
-            // Create kernel service
-            _kernelService = new KernelService(_kernel, _configuration);
-        }
+        // Create kernel service
+        _kernelService = new KernelService(_kernel, _configuration);
+    }
 
-        [Test]
-        public async Task CompleteChatStreamingAsync_ShouldReturnStreamingResults()
-        {
-            // Arrange
-            var messages = new List<ChatMessageContent>
+    [Test]
+    public async Task CompleteChatStreamingAsync_ShouldReturnStreamingResults()
+    {
+        // Arrange
+        var messages = new List<ChatMessageContent>
             {
                 new ChatMessageContent(AuthorRole.User, "Hello")
             };
 
-            // Create a list of expected streaming results
-            var expectedResults = new List<StreamingChatMessageContent>
+        // Create a list of expected streaming results
+        var expectedResults = new List<StreamingChatMessageContent>
             {
                 new StreamingChatMessageContent(AuthorRole.Assistant, "Hello"),
                 new StreamingChatMessageContent(AuthorRole.Assistant, " there"),
                 new StreamingChatMessageContent(AuthorRole.Assistant, "!")
             };
 
-            // Setup substitute chat completion service to return our expected results
-            _chatCompletionService
-                .GetStreamingChatMessageContentsAsync(
-                    Arg.Any<ChatHistory>(), 
-                    Arg.Any<PromptExecutionSettings>(), 
-                    Arg.Any<Kernel>(), 
-                    Arg.Any<CancellationToken>())
-                .Returns(expectedResults.ToAsyncEnumerable());
+        // Setup substitute chat completion service to return our expected results
+        _chatCompletionService
+            .GetStreamingChatMessageContentsAsync(
+                Arg.Any<ChatHistory>(),
+                Arg.Any<PromptExecutionSettings>(),
+                Arg.Any<Kernel>(),
+                Arg.Any<CancellationToken>())
+            .Returns(expectedResults.ToAsyncEnumerable());
 
-            // Act
-            var results = new List<string>();
-            await foreach (var result in _kernelService.CompleteChatStreamingAsync(messages))
-            {
-                results.Add(result);
-            }
-
-            // Assert
-            results.Count.ShouldBe(expectedResults.Count);
-            for (int i = 0; i < expectedResults.Count; i++)
-            {
-                results[i].ShouldBe(expectedResults[i].ToString());
-            }
-
+        // Act
+        var results = new List<string>();
+        await foreach (var result in _kernelService.CompleteChatStreamingAsync(messages))
+        {
+            results.Add(result);
         }
 
-        [Test]
-        public async Task CompleteChatStreamingAsync_ShouldHandleEmptyMessages()
+        // Assert
+        results.Count.ShouldBe(expectedResults.Count);
+        for (int i = 0; i < expectedResults.Count; i++)
         {
-            // Arrange
-            var messages = new List<ChatMessageContent>();
-            var expectedResults = new List<StreamingChatMessageContent>();
-
-            _chatCompletionService
-                .GetStreamingChatMessageContentsAsync(
-                    Arg.Any<ChatHistory>(),
-                    Arg.Any<PromptExecutionSettings>(),
-                    Arg.Any<Kernel>(),
-                    Arg.Any<CancellationToken>())
-                .Returns(expectedResults.ToAsyncEnumerable());
-
-            // Act
-            var results = new List<string>();
-            await foreach (var result in _kernelService.CompleteChatStreamingAsync(messages))
-            {
-                results.Add(result);
-            }
-
-            // Assert
-            results.ShouldBeEmpty();
+            results[i].ShouldBe(expectedResults[i].ToString());
         }
 
-        [Test]
-        public async Task CompleteChatStreamingAsync_ShouldHandleMultipleMessages()
+    }
+
+    [Test]
+    public async Task CompleteChatStreamingAsync_ShouldHandleEmptyMessages()
+    {
+        // Arrange
+        var messages = new List<ChatMessageContent>();
+        var expectedResults = new List<StreamingChatMessageContent>();
+
+        _chatCompletionService
+            .GetStreamingChatMessageContentsAsync(
+                Arg.Any<ChatHistory>(),
+                Arg.Any<PromptExecutionSettings>(),
+                Arg.Any<Kernel>(),
+                Arg.Any<CancellationToken>())
+            .Returns(expectedResults.ToAsyncEnumerable());
+
+        // Act
+        var results = new List<string>();
+        await foreach (var result in _kernelService.CompleteChatStreamingAsync(messages))
         {
-            // Arrange
-            var messages = new List<ChatMessageContent>
+            results.Add(result);
+        }
+
+        // Assert
+        results.ShouldBeEmpty();
+    }
+
+    [Test]
+    public async Task CompleteChatStreamingAsync_ShouldHandleMultipleMessages()
+    {
+        // Arrange
+        var messages = new List<ChatMessageContent>
             {
                 new ChatMessageContent(AuthorRole.User, "Hello"),
                 new ChatMessageContent(AuthorRole.Assistant, "Hi there!"),
                 new ChatMessageContent(AuthorRole.User, "How are you?")
             };
 
-            var expectedResults = new List<StreamingChatMessageContent>
+        var expectedResults = new List<StreamingChatMessageContent>
             {
                 new StreamingChatMessageContent(AuthorRole.Assistant, "I'm"),
                 new StreamingChatMessageContent(AuthorRole.Assistant, " doing"),
@@ -137,28 +137,27 @@ namespace InterviewAssistant.ApiService.Tests.Services
                 new StreamingChatMessageContent(AuthorRole.Assistant, " asking!")
             };
 
-            _chatCompletionService
-                .GetStreamingChatMessageContentsAsync(
-                    Arg.Any<ChatHistory>(),
-                    Arg.Any<PromptExecutionSettings>(),
-                    Arg.Any<Kernel>(),
-                    Arg.Any<CancellationToken>())
-                .Returns(expectedResults.ToAsyncEnumerable());
+        _chatCompletionService
+            .GetStreamingChatMessageContentsAsync(
+                Arg.Any<ChatHistory>(),
+                Arg.Any<PromptExecutionSettings>(),
+                Arg.Any<Kernel>(),
+                Arg.Any<CancellationToken>())
+            .Returns(expectedResults.ToAsyncEnumerable());
 
-            // Act
-            var results = new List<string>();
-            await foreach (var result in _kernelService.CompleteChatStreamingAsync(messages))
-            {
-                results.Add(result);
-            }
-
-            // Assert
-            results.Count.ShouldBe(expectedResults.Count);
-            for (int i = 0; i < expectedResults.Count; i++)
-            {
-                results[i].ShouldBe(expectedResults[i].ToString());
-            }
-
+        // Act
+        var results = new List<string>();
+        await foreach (var result in _kernelService.CompleteChatStreamingAsync(messages))
+        {
+            results.Add(result);
         }
+
+        // Assert
+        results.Count.ShouldBe(expectedResults.Count);
+        for (int i = 0; i < expectedResults.Count; i++)
+        {
+            results[i].ShouldBe(expectedResults[i].ToString());
+        }
+
     }
 }
