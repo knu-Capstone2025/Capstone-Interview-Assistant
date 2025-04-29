1bbc38b598e9ea77987baabe1fbafe98feb6dddc
dkswoals <a4445656@gmail.com>
2025-04-30 00:02:53 +0900

    Feat: 인메모리 저장소 구현

---
+++ src/InterviewAssistant.ApiService/Storage/Implementation/MemoryStorage.cs
@@ -0,0 +1,36 @@
+using InterviewAssistant.ApiService.Storage;
+
+namespace InterviewAssistant.ApiService.Storage;
+public class MemoryStorage : IMemoryStorage
+{
+    private readonly Dictionary<string, string> _storage;
+
+    public MemoryStorage()
+    {
+        _storage = new Dictionary<string, string>();
+    }
+
+    public void StoreContent(string key, string content)
+    {
+        _storage[key] = content;
+    }
+
+    public string GetContent(string key)
+    {
+        if (_storage.TryGetValue(key, out string content))
+        {
+            return content;
+        }
+        return null;
+    }
+
+    public IEnumerable<string> GetAllKeys()
+    {
+        return _storage.Keys;
+    }
+
+    public bool ContainsKey(string key)
+    {
+        return _storage.ContainsKey(key);
+    }
+}
