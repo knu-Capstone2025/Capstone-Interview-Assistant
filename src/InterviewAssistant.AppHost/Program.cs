902f18fed9461345afc8f536ee5bd5bcb5ceda9d
JIHO LEE <161289673+GitJIHO@users.noreply.github.com>
2025-05-11 17:35:28 +0900

    Deploy: Azure Application Insights를 활용한 애플리케이션 모니터링 기능 도입 (#53)
    * Chore: ServiceDefault에 OpenTelemetry 및 ApplicationInsights 설정 추가
    
    * Chore: 관련 패키지 추가
    
    * Deploy: ci-cd pipeline에 컨테이너 앱을 위한 환경변수 주입 로직 추가
    
    * Remove: 중복된 openAi 환경변수 삽입 로직 단일화
    
    * Remove: azure cli를 통한 환경변수 주입 로직 제거
    
    * Refactor: ServiceDefault 하위 Extensions.cs 내부 모니터링 추가 로직 간소화
    
    * Feat: 모니터링용 bicep 추가
    
    * Feat: 모니터링용 json 파일 추가
    
    * Feat: Azure Application Insights 모니터링 설정 배포/테스트 조건부 포함
    
    * Feat: 모니터링 자동 프로비저닝 방식으로 변경 및 테스트환경을 위한 환경변수 추가
    
    * Refactor: 명시적 bicep 삭제 및 배포 단계에서 모니터링 활성화하여 azd up 실행
    
    * Refactor: Application insights 관련 설정 깔끔한 방식으로 변경
    
    * Refactor: AppHost 하위 Program.cs 로직 정리
    
    * Refactor: Extensions.cs application ingishts 관련 로깅 제거 및 코드 단순화

--- src/InterviewAssistant.AppHost/Program.cs
+++ src/InterviewAssistant.AppHost/Program.cs
@@ -1,18 +1,24 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var openai = builder.AddConnectionString("openai");
+var insights = builder.ExecutionContext.IsPublishMode
+               ? builder.AddAzureApplicationInsights("applicationinsights")
+               : builder.AddConnectionString("applicationinsights");
 
+var openai = builder.AddConnectionString("openai");
 var config = builder.Configuration;
 
 var apiService = builder.AddProject<Projects.InterviewAssistant_ApiService>("apiservice")
                     .WithReference(openai)
+                    .WithReference(insights)
+                    .WaitFor(insights)
                     .WithEnvironment("SemanticKernel__ServiceId", config["SemanticKernel:ServiceId"]!)
                     .WithEnvironment("GitHub__Models__ModelId", config["GitHub:Models:ModelId"]!);
-
 
 builder.AddProject<Projects.InterviewAssistant_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
+    .WithReference(insights)
+    .WaitFor(insights)
     .WaitFor(apiService);
 
 builder.Build().Run();
